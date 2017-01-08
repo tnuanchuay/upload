03068d21515a171eac07acb337db0c0267505809
Tossapon Nuanchuay <ntossapo@gmail.com>
2017-01-08 20:01:41 +0700

    cliend mode

--- main.go
+++ main.go
@@ -7,28 +7,29 @@ import (
 	"log"
 	"io"
 	"os"
+	"io/ioutil"
+	"github.com/parnurzeal/gorequest"
+	"path/filepath"
 )
 
 var(
-	pathPtr		*string
 	path		string
-
 )
 
 func main(){
-	serveMode := flag.Bool("serve", false, "a bool")
-	pathPtr := flag.String("path", "/var/www/html", "a string")
-
+	serveMode := flag.Bool("serve", false, "serve mode")
+	pathPtr := flag.String("path", "/var/www/html", "specific path to save file")
+	server := flag.String("host", "", "a server to upload")
 	flag.Parse()
-	f, err := os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	log.SetOutput(f)
 
 	if *serveMode {
+		f, err := os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.SetOutput(f)
+
 		path = *pathPtr
 		if path[len(path) - 1] != '/'{
 			path += "/"
@@ -36,6 +37,39 @@ func main(){
 
 		http.HandleFunc("/", handler)
 		http.ListenAndServe(":54321", nil)
+	}else{
+		args := os.Args
+		if len(args) == 3{
+			filename := args[2]
+			host := fmt.Sprintf("http://%s:54321/", *server)
+
+			f, _ := filepath.Abs(filename)
+			byteOfFile, err := ioutil.ReadFile(f)
+			if err != nil {
+				log.Fatal("file err : ", err)
+			}
+
+			resp, body, errs := gorequest.New().
+				Post(host).
+				Type("multipart").
+				SendFile(byteOfFile, filename, "upload").
+				End()
+
+			if errs != nil{
+				for _, err := range errs{
+					log.Println("gorequest", err)
+				}
+				os.Exit(1)
+			}
+
+			if resp.StatusCode == 200{
+				fmt.Println(body)
+			}else{
+				fmt.Println("err", resp.StatusCode)
+			}
+		}else{
+			log.Fatal("usage : upload --host=127.0.0.1 file.jpg")
+		}
 	}
 }
 
